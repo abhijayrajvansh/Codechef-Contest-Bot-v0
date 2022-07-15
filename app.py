d7fd914043d0186ae431107357342e7d7502db4e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-07-15 05:55:35 +0530

    Initiated Codechef Contest Bot

--- app.py
+++ app.py
@@ -27,25 +27,22 @@ driver = webdriver.Chrome(service = PATH, options = chromeOptions)
 
 driver.get(url) # launches the broswer and open url
 
-
 # Building Testcase Downloader
 
 for i in range(1, 5 + 1):
-
-	CUSTOM_XPATH = "//span[@id='sample-" + "input" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
-
-	try:
-		sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH).text
-		print(sample_testcases)
-		print()
-	except:
-		print("No further Testcases was found!")
-		break
-
-	CUSTOM_XPATH = "//span[@id='sample-" + "output" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
-
-	sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH).text
-	print(sample_testcases)
     
+    try:
+        CUSTOM_XPATH_IN = "//span[@id='sample-" + "input" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
+        sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_IN).text
+        print(sample_testcases)
+        print()
+        CUSTOM_XPATH_OT = "//span[@id='sample-" + "output" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
+        sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_OT).text
+        print(sample_testcases)
+        print()
 
+    except:
+        print("No further Testcases was found!")
+        break
 
+    
