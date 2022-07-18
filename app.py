8c5a0947c6274873667bad0229dbd627fcd8d4f5
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-07-19 02:10:53 +0530

    COMPLETED

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from curses.ascii import isalnum, isalpha, isdigit
+from email import contentmanager
+from random import sample
 from time import sleep
 from typing import final
 from selenium import webdriver
@@ -10,15 +12,13 @@ import os
 pwd = "/Users/abhijayrajvansh/Downloads"
 PATH = Service(pwd + "/chromedriver")
 
-# URL to be launched ...
-URL = "https://www.codechef.com/problems-old/PRACLIST"
+# MULTIPLE_TT_ to be launched ...
+# MULTIPLE_TT_URL = "https://www.codechef.com/problems-old/PRACLIST"
 
 CODECHEF_URL = "https://www.codechef.com/"
 
 # CC Type to accept in argument
-url_cc_data = "LTIME108D"
-
-sample_url = "https://www.codechef.com/" + url_cc_data
+url_cc_data = "START47"
 
 # Handling Chrome Options:
 chromeOptions = Options()
@@ -31,26 +31,19 @@ driver = webdriver.Chrome(service = PATH, options = chromeOptions)
 # driver.maximize_window()
 # driver.minimize_window()
 
-test_url1 = "https://www.codechef.com/LTIME108D"
-test_url2 = "https://www.codechef.com/START47D"
-test_url3 = "https://www.codechef.com/COOK143D"
-test_url4 = "https://www.codechef.com/JULY221D"
+CONTEST_URL = "https://www.codechef.com/" + url_cc_data + "D"
+print("Looking for " + CONTEST_URL)
 
-driver.get(test_url2) # launches the broswer and open URL
+driver.get(CONTEST_URL) # launches the broswer and open MULTIPLE_TT_
 
-CONTEST_TYPE = "" #DONE 
-
-for i in url_cc_data:
-    if i.isdigit():
-        break
-    if i.isalpha():
-        CONTEST_TYPE += i
+CONTEST_TYPE = url_cc_data
 
 
 CC_Path = "/Users/abhijayrajvansh/Desktop/codechef/" + CONTEST_TYPE + "/"
 
 try : 
     os.mkdir(CC_Path)
+    print("\nCreating " + url_cc_data + " Directory...")
 except FileExistsError:
     print(end="")
 
@@ -77,71 +70,109 @@ problem_code = []
 for i in range(0, len(final_data), 4):
     problem_code.append(final_data[i+1])
 
-print(problem_code)
 
-#To be updated with live contest pre url of contest of each problem
+# #To be updated with live contest pre url of contest of each problem
 PRB_URL = "https://www.codechef.com/problems-old/"
 
 total_problems = len(problem_code)
 
+print("\nTotal problems found:", total_problems, "\n")
+print("All Problems Code: ", problem_code, "\n")
+
+
+def make_file(tc, curr_prob_path, count):
+    sample_file_name = "/sample_input_" + str(count) + ".txt"
+    open(curr_prob_path + sample_file_name, "w")
+    sample_file_name.write(tc[i]) 
+
+
+def total_testcases():
+    count = 0
+    for i in range(1, 5 + 1):
+        try:
+            CUSTOM_XPATH_IN = "//span[@id='sample-" + "input" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
+            sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_IN).text
+            count += 1
+        except:
+            break
+    
+    return count
 
 
 # Building Testcase Downloader
 def download_testcases(pb_code):
-    code = ""
-    for i in range(len(pb_code) - 2):
-        code += pb_code[i+2]
+    total_tc = total_testcases()
+    print("Total Testcases found: ", total_tc)
 
-    array_file = []
+    for tc_num in range(1, total_tc + 1):
+        strtc = str(tc_num)
+        CUSTOM_XPATH_IN = "//span[@id='sample-" + "input" + "-" + strtc + "']//pre[contains(@class,'mathjax-support')]"
+        sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_IN).text
 
-    for i in range(1, 5 + 1):   
-        try:
-            
-            CUSTOM_XPATH_IN = "//span[@id='sample-" + "input" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
-            sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_IN).text
-            print(sample_testcases)
+        arr = []     
+        temp = ""
 
+        sample_testcases += "\nAbhijay Rajvansh" # Edge testcases haha real life example
 
-            curr_prob_path = CC_Path + code
-            sample_file_name = "/sample_input_" + i + ".txt"
-            open(curr_prob_path + "/sample_input_" + i + ".txt", "w")
+        for i in sample_testcases:
+            if i == '\n':
+                arr.append(temp)
+                temp = ""
+            else:
+                temp += i
 
-            print()
-            
-            
-            CUSTOM_XPATH_OT = "//span[@id='sample-" + "output" + "-" + str(i) + "']//pre[contains(@class,'mathjax-support')]"
-            sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_OT).text
-            print(sample_testcases)
+        curr_prob_path = CC_Path + pb_code
+        print("Saving Testcase No:", strtc, "\n")
 
-            print()
-
-        except:
-            print("No further Testcases was found!")
-            break
-            
-    arr = []
-
-    n = len(arr)
-    file_num = -1
-    i = 0
-    while True:       
-        if i == n:
-            break
-
-        if arr[i] == "input" or arr[i] == "output":
-            i += 2
-            file_num += 1
-        
-        if file_num >= 0:
-            array_file[file_num].write(arr[i] + '\n')
-
-        i += 1
+        try : 
+            os.mkdir(curr_prob_path)
+        except FileExistsError:
+            print(end="")
 
 
+        with open(curr_prob_path + "/sample_input_" + strtc + ".txt", "w") as tc_file:
+            for line in arr:
+                tc_file.write(line + '\n')
 
-for i in range(total_problems):
-    driver.get(PRB_URL + problem_code[i])
+
+        CUSTOM_XPATH_OT = "//span[@id='sample-" + "output" + "-" + strtc + "']//pre[contains(@class,'mathjax-support')]"
+        sample_testcases = driver.find_element(By.XPATH, CUSTOM_XPATH_OT).text
+
+        arr = []     
+        temp = ""
+
+        sample_testcases += "\nAbhijay Rajvansh" # Edge testcases haha real life example
+
+        for i in sample_testcases:
+            if i == '\n':
+                arr.append(temp)
+                temp = ""
+            else:
+                temp += i
+
+        curr_prob_path = CC_Path + pb_code
+
+        try : 
+            os.mkdir(curr_prob_path)
+        except FileExistsError:
+            print(end="")
+
+
+        with open(curr_prob_path + "/sample_output_" + strtc + ".txt", "w") as tc_file:
+            for line in arr:
+                tc_file.write(line + '\n')
+
+
+for i in range(total_problems): #1 -> total_problems
+    curr_pb_code = problem_code[i]
+    pb_code = ""
+    for i in range(1, len(curr_pb_code)):
+        pb_code += curr_pb_code[i]
+
+    
+    print("Looking testcases for: ", PRB_URL + pb_code, "\n")
+    driver.get(PRB_URL + pb_code)
+
+    sleep(3)
+    download_testcases(pb_code)
     sleep(1)
-    download_testcases(problem_code[i])
-    sleep(1)
-
